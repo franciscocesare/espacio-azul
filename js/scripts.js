f865159c16cd864daa7419f54d6cff746282e9c1
franciscocesare <pako_cesare@hotmail.com>
2021-08-26 10:08:58 -0300

    slider en about, add youtube  link

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
 */
+
 (function ($) {
     "use strict"; // Start of use strict
 
